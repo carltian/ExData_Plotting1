bc388cb20e246c18cad41f4e5abe919d73d244b5
Xinchun Tian <tianxc@gmail.com>
2015-06-07 15:03:02 -0400

    Load the library of sqldf

--- plot2.R
+++ plot2.R
@@ -1,3 +1,4 @@
+library(sqldf)
 png(file="plot2.png",width=480,height=480)
 df <- read.csv.sql("household_power_consumption.txt",sql="select * from file where Date in ('1/2/2007','2/2/2007')",header=TRUE,sep=";")
 df$datetime <- paste(df$Date, df$Time)
