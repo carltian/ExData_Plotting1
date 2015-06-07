0232610aad5e37a4db10e2e72d42165fbda2e064
Xinchun Tian <tianxc@gmail.com>
2015-06-07 16:21:01 -0400

    Add the download file code

--- plot4.R
+++ plot4.R
@@ -1,4 +1,13 @@
 library(sqldf)
+if (!file.exists("household_power_consumption.txt")) {
+        if (!file.exists("household_power_consumption.zip")) {
+                url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+                download.file(url, destfile="./household_power_consumption.zip", method="curl")
+                unzip("household_power_consumption.zip")
+         } else {
+                unzip("household_power_consumption.zip")
+         }
+}
 png(file="plot4.png",width=480,height=480)
 df <- read.csv.sql("household_power_consumption.txt",sql="select * from file where Date in ('1/2/2007','2/2/2007')",header=TRUE,sep=";")
 df$datetime <- paste(df$Date, df$Time)
