57b86ecdb2d7dab1ae1bb366bab141229e00554f
Xinchun Tian <tianxc@gmail.com>
2015-06-07 16:57:18 -0400

    add the time that the file downloaded

--- plot1.R
+++ plot1.R
@@ -3,6 +3,7 @@ if (!file.exists("household_power_consumption.txt")) {
         if (!file.exists("household_power_consumption.zip")) {
                 url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
                 download.file(url, destfile="./household_power_consumption.zip", method="curl")
+		dateDownloaded <- date()
                 unzip("household_power_consumption.zip")
          } else {
   	        unzip("household_power_consumption.zip")
